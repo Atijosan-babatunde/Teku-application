ef3cb916b1745c7708517702ed8c5a95525a7526
olashina201 <olashina201@gmail.com>
2023-10-28 22:22:13 +0100

    fix inbox

--- src/component/dashboard/notification/newnotification.jsx
+++ src/component/dashboard/notification/newnotification.jsx
@@ -1,70 +1,65 @@
-import { useEffect, useState } from 'react';
-import styles from '../notification/css/newnotification.module.scss'
-import notification from '../../../assets/svg/notification.svg'
-import { useAppSelector } from '../../../shared/redux/reduxHooks';
-import { useDispatch } from 'react-redux';
-import { GetNotificationsData } from "../../../shared/redux/slices/notification.slices"
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useEffect, useState } from "react";
+import styles from "../notification/css/newnotification.module.scss";
+import notification from "../../../assets/svg/notification.svg";
+import { useAppSelector } from "../../../shared/redux/reduxHooks";
+import { useDispatch } from "react-redux";
+import { GetNotificationsData } from "../../../shared/redux/slices/notification.slices";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const NewNotification = () => {
-    const [loading, setLoading] = useState(false);
-    const dispatch = useDispatch();
-    const notificationData = useAppSelector((state) => state.notification.getDataNotifications)
-    const [data] = useState(notificationData)
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  const notificationData = useAppSelector(
+    (state) => state.notification.getDataNotifications
+  );
+  const [data] = useState(notificationData);
 
-    useEffect(() => {
-        getNotifications();
-    }, [data]);
+  useEffect(() => {
+    getNotifications();
+  }, [data]);
 
-    const getNotifications = () => {
-        setLoading(true);
-        dispatch(GetNotificationsData())
-            .unwrap()
-            .then(() => {
-                setLoading(false);
-            })
-            .catch((err) => {
-                toast.error(err, {
-                    position: toast.POSITION.TOP_RIGHT,
-                });
-                setLoading(false);
-            });
-    };
+  const getNotifications = () => {
+    setLoading(true);
+    dispatch(GetNotificationsData())
+      .unwrap()
+      .then(() => {
+        setLoading(false);
+      })
+      .catch((err) => {
+        toast.error(err, {
+          position: toast.POSITION.TOP_RIGHT,
+        });
+        setLoading(false);
+      });
+  };
 
-    const [notifications] = useState([
-        { id: 1, headermessage: 'Payment processing', bodymessage: 'Your payment has being processed. This may take up to 24hrs before confirmation.', date: '06:00am 16-05-2022' },
-        { id: 2, headermessage: 'Payment processing', bodymessage: 'Your payment has being processed. This may take up to 24hrs before confirmation.', date: '06:00am 16-05-2022' }
-    ])
-
-    if (data) {
-        return (
-            <div className={styles.parent}>
-                <div className={styles.content}>
-                    <>
-                        {data.map((prod, index) =>
-                            <div className={styles.contentinside} key={index}>
-                                <h1>{prod.title}</h1>
-                                <p>{prod.description}</p>
-                                <h2>{prod.createdAt}</h2>
-                            </div>
-                        )}
-                    </>
-                    <div className={styles.inner}>
-                        {data.length < 1 && (
-                            <div>
-                                <img src={notification} alt="middleimage" />
-                                <div className={styles.nocurrency}>
-                                    No notification
-                                </div>
-                            </div>
-                        )}
-                    </div>
-                </div>
-                <ToastContainer />
-            </div>
-        )
-    }
-}
+  if (data) {
+    return (
+      <div className={styles.parent}>
+        <div className={styles.content}>
+          <>
+            {data.map((prod, index) => (
+              <div className={styles.contentinside} key={index}>
+                <h1>{prod.title}</h1>
+                <p>{prod.description}</p>
+                <h2>{prod.createdAt}</h2>
+              </div>
+            ))}
+          </>
+          <div className={styles.inner}>
+            {data.length < 1 && (
+              <div>
+                <img src={notification} alt="middleimage" />
+                <div className={styles.nocurrency}>No notification</div>
+              </div>
+            )}
+          </div>
+        </div>
+        <ToastContainer />
+      </div>
+    );
+  }
+};
 
 export default NewNotification;
--- src/shared/redux/slices/notification.slices.jsx
+++ src/shared/redux/slices/notification.slices.jsx
@@ -2,43 +2,68 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { setMessage } from "./message.slices";
 import NotificationServices from "../services/notification.services";
 
-
-
 export const GetNotificationsData = createAsyncThunk(
-    "notification/getNotificationsData",
-    async (thunkAPI) => {
-        try {
-            const data = await NotificationServices.GetNotificationsData();
-            return { notification: data };
-        } catch (error) {
-            const message =
-                (error.response &&
-                    error.response.data &&
-                    error.response.data.message) ||
-                error.message ||
-                error.toString();
-            thunkAPI.dispatch(setMessage(message));
-            return thunkAPI.rejectWithValue(message);
-        }
+  "notification/getNotificationsData",
+  async (thunkAPI) => {
+    try {
+      const data = await NotificationServices.GetNotificationsData();
+      return { notification: data };
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      thunkAPI.dispatch(setMessage(message));
+      return thunkAPI.rejectWithValue(message);
     }
+  }
+);
+
+export const GetInboxData = createAsyncThunk(
+  "notification/getInboxData",
+  async (thunkAPI) => {
+    try {
+      const data = await NotificationServices.GetInboxData();
+      return { inbox: data };
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      thunkAPI.dispatch(setMessage(message));
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
 );
 
 const initialState = {
-    getDataNotifications: null,
+  getDataNotifications: null,
+  getInboxData: null,
 };
 
 export const notificationSlice = createSlice({
-    name: "notification",
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(GetNotificationsData.fulfilled, (state, action) => {
-            state.getDataNotifications = action.payload.notification;
-        })
-        builder.addCase(GetNotificationsData.rejected, (state) => {
-            state.getDataNotifications = null;
-        })
-    }
+  name: "notification",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(GetNotificationsData.fulfilled, (state, action) => {
+      state.getDataNotifications = action.payload.notification;
+    });
+    builder.addCase(GetNotificationsData.rejected, (state) => {
+      state.getDataNotifications = null;
+    });
+    
+    builder.addCase(GetInboxData.fulfilled, (state, action) => {
+      state.getInboxData = action.payload.inbox;
+    });
+    builder.addCase(GetInboxData.rejected, (state) => {
+      state.getInboxData = null;
+    });
+  },
 });
 
 const { reducer } = notificationSlice;
