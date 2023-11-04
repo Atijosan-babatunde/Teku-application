fee9225fc1bfb8d7f22f18e0f03019db37f3ee0f
olashina201 <olashina201@gmail.com>
2023-11-04 14:01:52 +0100

    update

--- src/shared/redux/rootReducer.jsx
+++ src/shared/redux/rootReducer.jsx
@@ -1,20 +1,18 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import landingReducer from './slices/landing.slices'
-import transactionReducer from './slices/transaction.slices'
-import recipientReducer from './slices/recipient.slices'
-import notificationReducer from './slices/notification.slices'
-import userReducer from './slices/users.slices'
-import kycReducer from './slices/kyc.slices'
-
-
+import { combineReducers } from "@reduxjs/toolkit";
+import landingReducer from "./slices/landing.slices";
+import transactionReducer from "./slices/transaction.slices";
+import recipientReducer from "./slices/recipient.slices";
+import notificationReducer from "./slices/notification.slices";
+import userReducer from "./slices/users.slices";
+import kycReducer from "./slices/kyc.slices";
 
 const rootReducer = combineReducers({
-    landing:landingReducer,
-    transaction:transactionReducer,
-    recipient:recipientReducer,
-    notification:notificationReducer,
-    users:userReducer,
-    kyc:kycReducer,
+  landing: landingReducer,
+  transaction: transactionReducer,
+  recipient: recipientReducer,
+  notification: notificationReducer,
+  users: userReducer,
+  kyc: kycReducer,
 });
 
-export default rootReducer
+export default rootReducer;
