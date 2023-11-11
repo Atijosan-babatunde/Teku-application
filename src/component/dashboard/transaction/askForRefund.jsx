135c551d69427f5c6e561835fda6de9db1b2f3d8
olashina201 <olashina201@gmail.com>
2023-11-12 00:28:44 +0100

    update

--- src/component/dashboard/transaction/askForRefund.jsx
+++ src/component/dashboard/transaction/askForRefund.jsx
@@ -1,129 +1,153 @@
-import styles from '../transaction/css/askforrefund.module.scss'
-import React, { useRef, useState } from "react"
-import useOnClickOutside from "../../../shared/Hooks/useOnClickOutside"
-import can from '../../../assets/png/can.png'
-import documentKYCIcon from '../../../assets/svg//documentKYC.svg'
-import ConfirmRefund from './confirmRefund'
+import styles from "../transaction/css/askforrefund.module.scss";
+import React, { useRef, useState } from "react";
+import useOnClickOutside from "../../../shared/Hooks/useOnClickOutside";
+import can from "../../../assets/png/can.png";
+import documentKYCIcon from "../../../assets/svg//documentKYC.svg";
+import ConfirmRefund from "./confirmRefund";
+import useCloudinaryImageUpload from "../../../shared/Hooks/useCloudinaryImageUpload";
+import customAxios from "../../../shared/utils/axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+const AskForRefund = ({ handleAskForRefund, selectedTransaction }) => {
+  const [loading, setLoading] = useState(false);
+  console.log(selectedTransaction);
+  const [formData, setFormData] = useState({
+    paymentDocument: "",
+    transactionId: selectedTransaction?.id ?? "",
+    paymentInstruction: "string",
+    reason: "string",
+  });
+  const [uploadImage] = useCloudinaryImageUpload();
 
+  const modalref = useRef();
+  useOnClickOutside(modalref, handleAskForRefund);
 
+  const validate = () => {
+    return !formData.paymentDocument || !formData.reason;
+  };
 
-const AskForRefund = ({ handleAskForRefund }) => {
-    const [appealDocument, setAppealDocument] = useState('')
-    const [refundReason, setRefundReason] = useState('')
-    const [filesName, setFilesName] = useState("");
-    const [formData, setFormData] = useState({});
+  const handleChangeDoc = async (event, name) => {
+    const fileUploaded = event.target.files[0];
+    setFormData({ ...formData, paymentDocument: fileUploaded });
+  };
 
-    const modalref = useRef()
-    useOnClickOutside(modalref, handleAskForRefund)
+  const handleClickDoc = () => {
+    documentdoc.current.click();
+  };
 
-    const validate = () => {
-        return !appealDocument || !refundReason
+  const documentdoc = useRef(null);
+
+  // // SHOW MODAL
+  const [showModalRefund, setShowModalRefund] = useState(false);
+  function handleModalConfirmRefund() {
+    setShowModalRefund(!showModalRefund);
+  }
+
+  const handleRequestRefund = async () => {
+    setLoading(true);
+    let secureUrl = "";
+    if (formData.paymentDocument) {
+      try {
+        secureUrl = await uploadImage(formData.paymentDocument);
+        console.log(secureUrl);
+        setFormData({ ...formData, paymentDocument: secureUrl });
+      } catch (error) {
+        console.error("Error uploading image to Cloudinary:", error);
+        setLoading(false);
+        return;
+      }
     }
 
-    const handleChangeDoc = async (event, name) => {
-        const fileUploaded = event.target.files[0];
-        setFilesName(fileUploaded.name);
-        getBase64(fileUploaded, async (result) => {
-            setFormData((curr) => {
-                return { ...curr, [name]: result };
-            });
-        });
-    };
-
-    const getBase64 = (file, cb) => {
-        let reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = function () {
-            cb(reader.result);
-        };
-        reader.onerror = function (error) {
-            console.log("Error: ", error);
-        };
-    };
-
-    const handleClickDoc = () => {
-        documentdoc.current.click();
-    };
-
-    const documentdoc = useRef(null);
-
-    // // SHOW MODAL
-    const [showModalRefund, setShowModalRefund] = useState(false)
-    function handleModalConfirmRefund() {
-        setShowModalRefund(!showModalRefund)
+    try {
+      const response = await customAxios.post(`/refund/request`, formData);
+      setLoading(false);
+      toast.success("Refund Request Sent Successfully", {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+      setShowModalRefund(!showModalRefund);
+    } catch (err) {
+      toast.error(err, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+      setLoading(false);
     }
+  };
 
-
-    return (
-        <div className={styles.parent}>
-            <div className={styles.content} ref={modalref}>
-                <div className={styles.closemodal} onClick={handleAskForRefund}>
-                    <img src={can} alt="close modal" />
-                </div>
-                <div className={styles.contentholder}>
-                    <p className={styles.pmodal}>Amount paid</p>
-                    <h1 className={styles.hmodal}>ZAR 50,000</h1>
-                </div>
-                <div className={styles.downcontent}>
-                    <div className={styles.addrefund}>Add refund account</div>
-                    <div className={styles.driverdoc}>
-                        <input
-                            type="file"
-                            accept=".png,.jpeg,.jpg,.doc,.docx,.pdf"
-                            ref={documentdoc}
-                            onChange={(e) => handleChangeDoc(e)}
-                            style={{ display: "none" }}
-                        />
-                        <img src={documentKYCIcon} alt="" />
-                        {filesName ? (<p onClick={handleClickDoc}>
-                            {filesName}
-                        </p>) : (<p onClick={handleClickDoc}>
-                            Tap to upload payment document/invoice. <br />
-                            <span> Maximum file size: 5mb</span>
-                        </p>)}
-                    </div>
-
-
-                    <h2 className={styles.rowname}>Payment instruction</h2>
-                    <textarea
-                        className={styles.calculatorinputtextarea}
-                        type="number"
-                        placeholder="Input recipient account details."
-                        cols="30"
-                        onChange={e => setAppealDocument(e.target.value)}
-                    />
-                    <p className={styles.green}>0/500</p>
-
-
-                    <h2 className={styles.rowname}>Reason for refund</h2>
-                    <textarea
-                        className={styles.calculatorinputtextarea}
-                        type="number"
-                        placeholder="Write here"
-                        cols="30"
-                        onChange={e => setRefundReason(e.target.value)}
-                    />
-                    <p className={styles.green}>0/500</p>
-
-
-
-
-                    <div className={styles.requestbut}>
-                        <button
-                            className={styles.btnrequest}
-                            disabled={validate()}
-                            onClick={handleModalConfirmRefund}
-                            style={{ backgroundColor: validate() ? "rgba(1, 27, 109, 0.20)" : " " }}
-                        >
-                            Send
-                        </button>
-                        {showModalRefund && <ConfirmRefund {...{ handleModalConfirmRefund }} />}
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className={styles.parent}>
+      <div className={styles.content} ref={modalref}>
+        <div className={styles.closemodal} onClick={handleAskForRefund}>
+          <img src={can} alt="close modal" />
         </div>
-    );
-}
+        <div className={styles.contentholder}>
+          <p className={styles.pmodal}>Amount paid</p>
+          <h1 className={styles.hmodal}>{selectedTransaction.baseCurrency.code} {selectedTransaction.amount}</h1>
+        </div>
+        <div className={styles.downcontent}>
+          <div className={styles.addrefund}>Add refund account</div>
+          <div className={styles.driverdoc}>
+            <input
+              type="file"
+              accept=".png,.jpeg,.jpg,.doc,.docx,.pdf"
+              ref={documentdoc}
+              onChange={(e) => handleChangeDoc(e)}
+              style={{ display: "none" }}
+            />
+            <img src={documentKYCIcon} alt="" />
+            {formData.paymentDocument ? (
+              <p onClick={handleClickDoc}>{formData.paymentDocument.name}</p>
+            ) : (
+              <p onClick={handleClickDoc}>
+                Tap to upload payment document/invoice. <br />
+                <span> Maximum file size: 5mb</span>
+              </p>
+            )}
+          </div>
+
+          <h2 className={styles.rowname}>Payment instruction</h2>
+          <textarea
+            className={styles.calculatorinputtextarea}
+            type="text"
+            placeholder="Input recipient account details."
+            cols="30"
+            onChange={(e) =>
+              setFormData({ ...formData, paymentInstruction: e.target.value })
+            }
+          />
+          <p className={styles.green}>0/500</p>
+
+          <h2 className={styles.rowname}>Reason for refund</h2>
+          <textarea
+            className={styles.calculatorinputtextarea}
+            type="text"
+            placeholder="Write here"
+            cols="30"
+            onChange={(e) =>
+              setFormData({ ...formData, reason: e.target.value })
+            }
+          />
+          <p className={styles.green}>0/500</p>
+
+          <div className={styles.requestbut}>
+            <button
+              className={styles.btnrequest}
+              disabled={validate()}
+              onClick={handleRequestRefund}
+              style={{
+                backgroundColor: validate() ? "rgba(1, 27, 109, 0.20)" : " ",
+              }}
+            >
+              Send
+            </button>
+            {showModalRefund && (
+              <ConfirmRefund {...{ handleModalConfirmRefund }} selectedTransaction={selectedTransaction} />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AskForRefund;
--- src/component/dashboard/transaction/transactionPreviewModal.jsx
+++ src/component/dashboard/transaction/transactionPreviewModal.jsx
@@ -7,12 +7,15 @@ import download from "../../../assets/svg/download.svg";
 import shear from "../../../assets/svg/shearimg.svg";
 import AskForRefund from "./askForRefund";
 
-const TransactionPreview = ({ handleModalShowTransactionPreview, selectedTransaction }) => {
+const TransactionPreview = ({
+  handleModalShowTransactionPreview,
+  selectedTransaction,
+}) => {
   const [showThis, setShowThis] = useState(false);
   const modalref = useRef();
   useOnClickOutside(modalref, handleModalShowTransactionPreview);
 
-  console.log(selectedTransaction)
+  console.log(selectedTransaction);
   // // SHOW MODAL
   const [showModalAskRefund, setShowModalAskRefund] = useState(false);
   function handleAskForRefund() {
@@ -24,40 +27,40 @@ const TransactionPreview = ({ handleModalShowTransactionPreview, selectedTransac
     return totalAmount;
   };
 
-//   {
-//     "id": "1b9b4450-d305-44ed-8bef-8a751a5d9730",
-//     "baseCurrencyId": "213676cd-a1dc-4b2a-a911-9938ddd8ac0d",
-//     "pairCurrencyId": "1baf6c25-ef17-48cf-8ee6-272db2d711e2",
-//     "amount": 1000,
-//     "country": "Åland Islands",
-//     "purpose": "Food bills",
-//     "status": "PROCESSING",
-//     "paymentDocument": "https://res.cloudinary.com/dvcjgxou9/image/upload/v1699740336/xkys0twipp9sirwhdwzr.png",
-//     "paymentInstruction": "bhjbafhjbwjhf",
-//     "paymentDescription": "vvhgv",
-//     "paymentMethod": "BANK_TRANSFER",
-//     "bankName": "Renner and Sons",
-//     "confirmation": "https://res.cloudinary.com/dvcjgxou9/image/upload/v1699740337/i3przjbs1wjktwq5vxrz.png",
-//     "ownerId": "4bda1953-0ab4-49af-9107-08223e1926fb",
-//     "createdAt": "2023-11-11T22:05:37.706Z",
-//     "updatedAt": "2023-11-11T22:05:37.706Z",
-//     "baseCurrency": {
-//         "id": "213676cd-a1dc-4b2a-a911-9938ddd8ac0d",
-//         "icon": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1084.jpg",
-//         "code": "IDR",
-//         "name": "Somali Shilling",
-//         "createdAt": "2023-11-11T21:07:57.374Z",
-//         "updatedAt": "2023-11-11T21:07:57.374Z"
-//     },
-//     "pairCurrency": {
-//         "id": "1baf6c25-ef17-48cf-8ee6-272db2d711e2",
-//         "icon": "https://avatars.githubusercontent.com/u/76705902",
-//         "code": "DKK",
-//         "name": "Lilangeni",
-//         "createdAt": "2023-11-11T21:07:57.505Z",
-//         "updatedAt": "2023-11-11T21:07:57.505Z"
-//     }
-// }
+  //   {
+  //     "id": "1b9b4450-d305-44ed-8bef-8a751a5d9730",
+  //     "baseCurrencyId": "213676cd-a1dc-4b2a-a911-9938ddd8ac0d",
+  //     "pairCurrencyId": "1baf6c25-ef17-48cf-8ee6-272db2d711e2",
+  //     "amount": 1000,
+  //     "country": "Åland Islands",
+  //     "purpose": "Food bills",
+  //     "status": "PROCESSING",
+  //     "paymentDocument": "https://res.cloudinary.com/dvcjgxou9/image/upload/v1699740336/xkys0twipp9sirwhdwzr.png",
+  //     "paymentInstruction": "bhjbafhjbwjhf",
+  //     "paymentDescription": "vvhgv",
+  //     "paymentMethod": "BANK_TRANSFER",
+  //     "bankName": "Renner and Sons",
+  //     "confirmation": "https://res.cloudinary.com/dvcjgxou9/image/upload/v1699740337/i3przjbs1wjktwq5vxrz.png",
+  //     "ownerId": "4bda1953-0ab4-49af-9107-08223e1926fb",
+  //     "createdAt": "2023-11-11T22:05:37.706Z",
+  //     "updatedAt": "2023-11-11T22:05:37.706Z",
+  //     "baseCurrency": {
+  //         "id": "213676cd-a1dc-4b2a-a911-9938ddd8ac0d",
+  //         "icon": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1084.jpg",
+  //         "code": "IDR",
+  //         "name": "Somali Shilling",
+  //         "createdAt": "2023-11-11T21:07:57.374Z",
+  //         "updatedAt": "2023-11-11T21:07:57.374Z"
+  //     },
+  //     "pairCurrency": {
+  //         "id": "1baf6c25-ef17-48cf-8ee6-272db2d711e2",
+  //         "icon": "https://avatars.githubusercontent.com/u/76705902",
+  //         "code": "DKK",
+  //         "name": "Lilangeni",
+  //         "createdAt": "2023-11-11T21:07:57.505Z",
+  //         "updatedAt": "2023-11-11T21:07:57.505Z"
+  //     }
+  // }
 
   return (
     <div className={styles.parent}>
@@ -74,36 +77,51 @@ const TransactionPreview = ({ handleModalShowTransactionPreview, selectedTransac
         </div>
         <div className={styles.downcontent}>
           <p className={styles.ptag}>Amount to be paid:</p>
-          <h2 className={styles.h2tag}>{selectedTransaction.baseCurrency.code} {selectedTransaction.amount}</h2>
+          <h2 className={styles.h2tag}>
+            {selectedTransaction.baseCurrency.code} {selectedTransaction.amount}
+          </h2>
 
           <div className={styles.firstdiv}>
             <div className={styles.firstdivflex}>
               <div className={styles.firstdivh1}>To:</div>
-              <div className={styles.firstdivpbold}>{selectedTransaction?.paymentDescription}</div>
+              <div className={styles.firstdivpbold}>
+                {selectedTransaction?.paymentDescription}
+              </div>
             </div>
             <div className={styles.firstdivflex}>
               <div className={styles.firstdivh1}>Date:</div>
-              <div className={styles.firstdivpbold}>{selectedTransaction.createdAt}</div>
+              <div className={styles.firstdivpbold}>
+                {selectedTransaction.createdAt}
+              </div>
             </div>
 
             <div className={styles.firstdivflex}>
               <div className={styles.firstdivh1}>Time:</div>
-              <div className={styles.firstdivpbold}>{selectedTransaction.createdAt}</div>
+              <div className={styles.firstdivpbold}>
+                {selectedTransaction.createdAt}
+              </div>
             </div>
 
             <div className={styles.firstdivflex}>
               <div className={styles.firstdivh1}>Bank:</div>
-              <div className={styles.firstdivp}>{selectedTransaction.bankName}</div>
+              <div className={styles.firstdivp}>
+                {selectedTransaction.bankName}
+              </div>
             </div>
 
             <div className={styles.firstdivflex}>
               <div className={styles.firstdivh1}>Payment method:</div>
-              <div className={styles.firstdivp}>{selectedTransaction.paymentMethod}</div>
+              <div className={styles.firstdivp}>
+                {selectedTransaction.paymentMethod}
+              </div>
             </div>
 
             <div className={styles.firstdivflex}>
               <div className={styles.firstdivh1}>Transfer charge:</div>
-              <div className={styles.firstdivp}>{selectedTransaction.baseCurrency.code} getAmountPercentage(selectedTransaction.amount)</div>
+              <div className={styles.firstdivp}>
+                {selectedTransaction.baseCurrency.code}{" "}
+                getAmountPercentage(selectedTransaction.amount)
+              </div>
             </div>
 
             <div className={styles.firstdivflex}>
@@ -126,42 +144,58 @@ const TransactionPreview = ({ handleModalShowTransactionPreview, selectedTransac
                 </div>
                 <div className={styles.firstdivflex}>
                   <div className={styles.firstdivh1}>Country:</div>
-                  <div className={styles.firstdivpbold}>{selectedTransaction.country}</div>
+                  <div className={styles.firstdivpbold}>
+                    {selectedTransaction.country}
+                  </div>
                 </div>
 
                 <div className={styles.firstdivflex}>
                   <div className={styles.firstdivh1}>Delivery date:</div>
-                  <div className={styles.firstdivpbold}>{selectedTransaction.updatedAt}</div>
+                  <div className={styles.firstdivpbold}>
+                    {selectedTransaction.updatedAt}
+                  </div>
                 </div>
 
                 <div className={styles.firstdivflex}>
                   <div className={styles.firstdivh1}>Delivery time:</div>
-                  <div className={styles.firstdivp}>{selectedTransaction.updatedAt}</div>
+                  <div className={styles.firstdivp}>
+                    {selectedTransaction.updatedAt}
+                  </div>
                 </div>
 
                 <div className={styles.firstdivflex}>
                   <div className={styles.firstdivh1}>Purpose of payment:</div>
-                  <div className={styles.firstdivp}>{selectedTransaction.purpose}</div>
+                  <div className={styles.firstdivp}>
+                    {selectedTransaction.purpose}
+                  </div>
                 </div>
 
                 <div className={styles.firstdivflex}>
                   <div className={styles.firstdivh1}>Payment description:</div>
-                  <div className={styles.firstdivp}>{selectedTransaction.paymentDescription}</div>
+                  <div className={styles.firstdivp}>
+                    {selectedTransaction.paymentDescription}
+                  </div>
                 </div>
 
                 <div className={styles.firstdivflex}>
                   <div className={styles.firstdivh1}>Sending method:</div>
-                  <div className={styles.firstdivp}>{selectedTransaction.paymentMethod}</div>
+                  <div className={styles.firstdivp}>
+                    {selectedTransaction.paymentMethod}
+                  </div>
                 </div>
 
                 <div className={styles.firstdivflex}>
                   <div className={styles.firstdivh1}>Currency pair:</div>
-                  <div className={styles.firstdivp}>{`${selectedTransaction.baseCurrency.code} / ${selectedTransaction.pairCurrency.code}`}</div>
+                  <div
+                    className={styles.firstdivp}
+                  >{`${selectedTransaction.baseCurrency.code} / ${selectedTransaction.pairCurrency.code}`}</div>
                 </div>
 
                 <div className={styles.firstdivflex}>
                   <div className={styles.firstdivh1}>Transaction ID:</div>
-                  <div className={styles.firstdivp}>{selectedTransaction.id}</div>
+                  <div className={styles.firstdivp}>
+                    {selectedTransaction.id}
+                  </div>
                 </div>
               </div>
               <button className={styles.firstbtndown}>
@@ -197,7 +231,12 @@ const TransactionPreview = ({ handleModalShowTransactionPreview, selectedTransac
             >
               Ask for refund
             </button>
-            {showModalAskRefund && <AskForRefund {...{ handleAskForRefund }} />}
+            {showModalAskRefund && (
+              <AskForRefund
+                {...{ handleAskForRefund }}
+                selectedTransaction={selectedTransaction}
+              />
+            )}
           </div>
 
           <div className={styles.btnflex}>
