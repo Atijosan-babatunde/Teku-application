a9624078de7ad23d5c1da85798e635a4573c29b4
olashina201 <olashina201@gmail.com>
2024-01-21 09:57:03 +0100

    update

--- src/component/herosectiontable/TodayRate/todayRate.jsx
+++ src/component/herosectiontable/TodayRate/todayRate.jsx
@@ -1,124 +1,192 @@
-import styles from "../TodayRate/todayrate.module.scss"
-import { BsFillSendFill } from 'react-icons/bs';
-import { useState, useEffect } from "react"
-import { useDispatch } from 'react-redux';
-import { GetCurrencyPair } from "../../../shared/redux/slices/landing.slices"
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useNavigate } from "react-router-dom"
-import { MdKeyboardArrowRight } from 'react-icons/md'
-import { useAppSelector } from '../../../shared/redux/reduxHooks';
-import international from "../../../assets/svg/international.svg"
+import styles from "../TodayRate/todayrate.module.scss";
+import { BsFillSendFill } from "react-icons/bs";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { GetCurrencyPair } from "../../../shared/redux/slices/landing.slices";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { useAppSelector } from "../../../shared/redux/reduxHooks";
+import international from "../../../assets/svg/international.svg";
 import ReactLoading from "react-loading";
-
-
-
+import { formatMoney } from "../../../shared/utils/moneyFormat";
 
 const TodayRate = () => {
-    const navigate = useNavigate()
-    const [loading, setLoading] = useState(false);
-    const dispatch = useDispatch();
-    const currencyData = useAppSelector((state) => state.landing.getAllCurrencyData)
-    const [data] = useState(currencyData)
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  const currencyData = useAppSelector(
+    (state) => state.landing.getAllCurrencyData
+  );
+  const [data] = useState(currencyData);
 
-    useEffect(() => {
-        getCurrencyPair();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [data]);
+  useEffect(() => {
+    getCurrencyPair();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
 
+  const getCurrencyPair = () => {
+    setLoading(true);
+    dispatch(GetCurrencyPair())
+      .unwrap()
+      .then(() => {
+        setLoading(false);
+      })
+      .catch((err) => {
+        toast.error(err, {
+          position: toast.POSITION.TOP_RIGHT,
+        });
+        setLoading(false);
+      });
+  };
 
-    const getCurrencyPair = () => {
-        setLoading(true);
-        dispatch(GetCurrencyPair())
-            .unwrap()
-            .then(() => {
-                setLoading(false);
-            })
-            .catch((err) => {
-                toast.error(err, {
-                    position: toast.POSITION.TOP_RIGHT,
-                });
-                setLoading(false);
-            });
-    };
+  const seeAllTodayRate = () => {
+    navigate("/see-all");
+  };
 
-    const seeAllTodayRate = () => {
-        navigate("/see-all")
-    }
+  const goToLogin = () => {
+    navigate("/login");
+  };
 
-    const goToLogin = () => {
-        navigate("/login")
-    }
-
-    if (data) {
-        return (
-            <div className={styles.parent}>
-                <div className={styles.content}>
-                    {/* <div className={styles.loader}>
+  if (data) {
+    return (
+      <div className={styles.parent}>
+        <div className={styles.content}>
+          {/* <div className={styles.loader}>
                     {loading && (
                         <ReactLoading color="blue" width={20} height={20} type="spin" />
                     )}
                 </div> */}
-                    <div className="table-responsive">
-                        <table className="table table-striped table-borderless">
-                            <thead className={styles.tablerow}>
-                                <tr>
-                                    <th className={styles.tablehead} scope="col" style={{ paddingLeft: "2em", paddingBottom: "1.5000em" }}>Currency Pair</th>
-                                    <th className={styles.tablehead} scope="col" style={{ paddingLeft: "2em", paddingBottom: "1.5000em" }}>Rate</th>
-                                    <th className={styles.tablehead} scope="col" style={{ paddingBottom: "1.5000em" }}>Available Amount</th>
-                                    <th className={styles.tablehead} scope="col" style={{ paddingBottom: "1.5000em" }}>Sending Method</th>
-                                    <th className={styles.tablehead} scope="col" style={{ paddingBottom: "1.5000em" }}>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {data.map((prod, index) =>
-                                    <tr style={{}} key={index}>
-                                        <td className={styles.tabledata} style={{ paddingLeft: "2em", paddingTop: "1.5000em" }}>
-                                            <img src={prod.baseCurrency.icon} alt="" className={styles.flagstyle} />
-                                            <span className={styles.flagnamestyle} >{prod.baseCurrency.code}</span>
-                                            <span className={styles.dash}>-</span>
-                                            <img src={prod.pairCurrency.icon} alt="" className={styles.flagstyle} />
-                                            <span className={styles.flagnamestyle}>{prod.pairCurrency.code}</span>
-                                        </td>
-                                        <td className={styles.tabledata} style={{ paddingLeft: "2em", paddingTop: "1.5000em" }}>{prod.rate}</td>
-                                        <td className={styles.tabledata} style={{ paddingTop: "1.5000em" }}>{prod.availableAmount}</td>
-                                        <td className={styles.tabledata} style={{ paddingTop: "1.5000em" }}>
-                                            {prod.method}
-                                            <div className={styles.tableparagraph} style={{textTransform: "capitalize"}}>
-                                                {prod.sendingMethod.split('_')?.join(' ').toLowerCase()}
-                                            </div>
-                                        </td>
-                                        <td className={styles.tabledata} style={{ paddingTop: "1em" }}>
-                                            <button className={styles.btn} onClick={goToLogin}>Request <BsFillSendFill /></button>
-                                        </td>
-                                    </tr>
-                                )}
-
-                            </tbody>
-                        </table>
-                        {loading && (
-                            <ReactLoading color="blue" width={25} height={25} type="spin" />
-                        )}
-                        <div className={styles.dimbutton} onClick={seeAllTodayRate}>
-                            <button className={styles.seeallbut}>See all<MdKeyboardArrowRight /></button>
-                        </div>
-
-                        <div className={styles.inner}>
-                            {data.length < 1 && (
-                                <div>
-                                    <img src={international} alt="middleimage" />
-                                    <div className={styles.nocurrency}>
-                                        No rate today
-                                    </div>
-                                </div>
-                            )}
-                        </div>
-                    </div>
-                </div>
-                <ToastContainer />
+          <div className="table-responsive">
+            <table className="table table-striped table-borderless">
+              <thead className={styles.tablerow}>
+                <tr>
+                  <th
+                    className={styles.tablehead}
+                    scope="col"
+                    style={{ paddingLeft: "2em", paddingBottom: "1.5000em" }}
+                  >
+                    Currency Pair
+                  </th>
+                  <th
+                    className={styles.tablehead}
+                    scope="col"
+                    style={{ paddingLeft: "2em", paddingBottom: "1.5000em" }}
+                  >
+                    Rate
+                  </th>
+                  <th
+                    className={styles.tablehead}
+                    scope="col"
+                    style={{ paddingBottom: "1.5000em" }}
+                  >
+                    Available Amount
+                  </th>
+                  <th
+                    className={styles.tablehead}
+                    scope="col"
+                    style={{ paddingBottom: "1.5000em" }}
+                  >
+                    Sending Method
+                  </th>
+                  <th
+                    className={styles.tablehead}
+                    scope="col"
+                    style={{ paddingBottom: "1.5000em" }}
+                  >
+                    Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((prod, index) => (
+                  <tr style={{}} key={index}>
+                    <td
+                      className={styles.tabledata}
+                      style={{ paddingLeft: "2em", paddingTop: "1.5000em" }}
+                    >
+                      <img
+                        src={prod.baseCurrency.icon}
+                        alt=""
+                        className={styles.flagstyle}
+                      />
+                      <span className={styles.flagnamestyle}>
+                        {prod.baseCurrency.code}
+                      </span>
+                      <span className={styles.dash}>-</span>
+                      <img
+                        src={prod.pairCurrency.icon}
+                        alt=""
+                        className={styles.flagstyle}
+                      />
+                      <span className={styles.flagnamestyle}>
+                        {prod.pairCurrency.code}
+                      </span>
+                    </td>
+                    <td
+                      className={styles.tabledata}
+                      style={{ paddingLeft: "2em", paddingTop: "1.5000em" }}
+                    >
+                      {formatMoney(prod.rate, prod.pairCurrency.code)}
+                    </td>
+                    <td
+                      className={styles.tabledata}
+                      style={{ paddingTop: "1.5000em" }}
+                    >
+                      {formatMoney(
+                        prod.availableAmount,
+                        prod.baseCurrency.code
+                      )}
+                    </td>
+                    <td
+                      className={styles.tabledata}
+                      style={{ paddingTop: "1.5000em" }}
+                    >
+                      {prod.method}
+                      <div
+                        className={styles.tableparagraph}
+                        style={{ textTransform: "capitalize" }}
+                      >
+                        {prod.sendingMethod.split("_")?.join(" ").toLowerCase()}
+                      </div>
+                    </td>
+                    <td
+                      className={styles.tabledata}
+                      style={{ paddingTop: "1em" }}
+                    >
+                      <button className={styles.btn} onClick={goToLogin}>
+                        Request <BsFillSendFill />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            {loading && (
+              <ReactLoading color="blue" width={25} height={25} type="spin" />
+            )}
+            <div className={styles.dimbutton} onClick={seeAllTodayRate}>
+              <button className={styles.seeallbut}>
+                See all
+                <MdKeyboardArrowRight />
+              </button>
             </div>
-        );
-    }
-}
+
+            <div className={styles.inner}>
+              {data.length < 1 && (
+                <div>
+                  <img src={international} alt="middleimage" />
+                  <div className={styles.nocurrency}>No rate today</div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+        <ToastContainer />
+      </div>
+    );
+  }
+};
 
 export default TodayRate;
--- src/component/herosectiontable/herosectiontable.jsx
+++ src/component/herosectiontable/herosectiontable.jsx
@@ -1,61 +1,91 @@
-import styles from "../herosectiontable/herosectiontable.module.scss"
-import hot from "../../assets/svg/hot.svg"
-import ringing from "../../assets/svg/ringing.svg"
-import request from "../../assets/svg/request.png"
-import { FiSearch } from 'react-icons/fi'
-import { useState } from 'react';
-import TodayRate from "./TodayRate/todayRate"
-import CurrencyAlert from "./CurrencyAlert/currencyAlert"
-import CustomRequest from "./CustomRequest/customRequest"
-
-
+import styles from "../herosectiontable/herosectiontable.module.scss";
+import hot from "../../assets/svg/hot.svg";
+import ringing from "../../assets/svg/ringing.svg";
+import request from "../../assets/svg/request.png";
+import { FiSearch } from "react-icons/fi";
+import { useState } from "react";
+import TodayRate from "./TodayRate/todayRate";
+import CurrencyAlert from "./CurrencyAlert/currencyAlert";
+import CustomRequest from "./CustomRequest/customRequest";
 
 const Herosectiontable = () => {
-    const [showTodayRate, setShowTodayRate] = useState(true)
-    const [showCurrencyAlert, setShowCurrencyAlert] = useState(false)
-    const [showCustomRequest, setShowCustomRequest] = useState(false)
+  const [showTodayRate, setShowTodayRate] = useState(true);
+  const [showCurrencyAlert, setShowCurrencyAlert] = useState(false);
+  const [showCustomRequest, setShowCustomRequest] = useState(false);
 
-    const gotoTodayRate = () => {
-        setShowTodayRate(true)
-        setShowCurrencyAlert(false)
-        setShowCustomRequest(false)
-    }
-    const gotoCurrency = () => {
-        setShowCurrencyAlert(true)
-        setShowTodayRate(false)
-        setShowCustomRequest(false)
-    }
-    const gotoRequest = () => {
-        setShowCustomRequest(true)
-        setShowCurrencyAlert(false)
-        setShowTodayRate(false)
-    }
-   
+  const gotoTodayRate = () => {
+    setShowTodayRate(true);
+    setShowCurrencyAlert(false);
+    setShowCustomRequest(false);
+  };
+  const gotoCurrency = () => {
+    setShowCurrencyAlert(true);
+    setShowTodayRate(false);
+    setShowCustomRequest(false);
+  };
+  const gotoRequest = () => {
+    setShowCustomRequest(true);
+    setShowCurrencyAlert(false);
+    setShowTodayRate(false);
+  };
 
-    return (
-        <div className={styles.parent}>
-            <div className={styles.content}>
-                <div className={styles.buttonrow}>
-                    <div className={styles.buttonhead} onClick={gotoTodayRate} style={{ backgroundColor: showTodayRate ? "#fff" : "", color: showTodayRate ? "#000" : ""}}><img src={hot} alt="hot" />Today's Rate</div>
-                    <div className={styles.buttonhead} onClick={gotoCurrency} style={{ backgroundColor: showCurrencyAlert ? "#fff" : "", color: showCurrencyAlert ? "#000" : ""}}><img src={ringing} alt="ringing" />Currency Alert</div>
-                    <div className={styles.buttonhead} onClick={gotoRequest} style={{ backgroundColor: showCustomRequest ? "#fff" : "", color: showCustomRequest ? "#000" : "", paddingRight: "3em"}}><img src={request} alt="request" />Make a custom request</div>
-                    <div className={styles.search}>
-                        <input type="text" placeholder="Search" />
-                        <span><FiSearch /></span>
-                    </div>
-                </div>
-            </div>
-            <div style={{ display: showTodayRate ? '' : 'none'}}>
-                <TodayRate />
-            </div>
-            <div style={{ display: showCurrencyAlert ? '' : 'none' }}>
-                <CurrencyAlert />
-            </div>
-            <div style={{ display: showCustomRequest ? '' : 'none' }}>
-                <CustomRequest />
-            </div>
+  return (
+    <div className={styles.parent}>
+      <div className={styles.content}>
+        <div className={styles.buttonrow}>
+          <div
+            className={styles.buttonhead}
+            onClick={gotoTodayRate}
+            style={{
+              backgroundColor: showTodayRate ? "#fff" : "",
+              color: showTodayRate ? "#000" : "",
+            }}
+          >
+            <img src={hot} alt="hot" />
+            Today's Rate
+          </div>
+          <div
+            className={styles.buttonhead}
+            onClick={gotoCurrency}
+            style={{
+              backgroundColor: showCurrencyAlert ? "#fff" : "",
+              color: showCurrencyAlert ? "#000" : "",
+            }}
+          >
+            <img src={ringing} alt="ringing" />
+            Currency Alert
+          </div>
+          <div
+            className={styles.buttonhead}
+            onClick={gotoRequest}
+            style={{
+              backgroundColor: showCustomRequest ? "#fff" : "",
+              color: showCustomRequest ? "#000" : "",
+              paddingRight: "3em",
+            }}
+          >
+            <img src={request} alt="request" />
+            Make a custom request
+          </div>
+          <div className={styles.search}>
+            <input type="text" placeholder="Search" />
+            <span>
+              <FiSearch />
+            </span>
+          </div>
         </div>
-    );
-}
+      </div>
+      <div style={{ display: showTodayRate ? "" : "none" }}>
+        <TodayRate />
+      </div>
+      <div style={{ display: showCurrencyAlert ? "" : "none" }}>
+        <CurrencyAlert />
+      </div>
+      <div style={{ display: showCustomRequest ? "" : "none" }}>
+        <CustomRequest />
+      </div>
+    </div>
+  );
+};
 
 export default Herosectiontable;
